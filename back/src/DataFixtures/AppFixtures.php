5a3d0b7d4c31bfdf34c04db72d6e6d7c2796843f
Layeche-t <layeche.torki@gmail.com>
2023-02-26 15:28:14 +0100

    integration of data into the database

---
+++ back/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public string $projectDir; 
+
+    public function __construct(string $projectDir) 
+    { 
+      $this->projectDir = $projectDir;
+    } 
+
+
+    public function load(ObjectManager $manager): void
+    {
+        
+        $data = json_decode(file_get_contents($this->projectDir. '/public/products.json')); 
+
+        
+
+        foreach ($data->data as $value) {
+
+            $product = new Product(); 
+
+           
+            $product->setId($value->id); 
+            $product->setCode($value->code); 
+            $product->setName($value->name); 
+            $product->setDescription($value->description); 
+            $product->setImage($value->image);
+            $product->setPrice($value->price); 
+            $product->setCategory($value->category); 
+            $product->setQuantity($value->quantity); 
+            $product->setInventoryStatus($value->inventoryStatus); 
+            $product->setRating($value->rating); 
+
+            $manager->persist($product);
+        }
+
+        
+        
+
+        $manager->flush();
+    }
+}
--- back/src/Entity/Product.php
+++ back/src/Entity/Product.php
@@ -45,6 +45,13 @@ class Product
         return $this->id;
     }
 
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getCode(): ?string
     {
         return $this->code;
