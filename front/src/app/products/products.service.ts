62277ef4cb59e9dffc2df08a3d2879d31c0bf1b1
Layeche-t <85928199+Layeche-t@users.noreply.github.com>
2023-02-27 15:08:48 +0100

    Update products.service.ts
    link the operations of the front with the back

--- front/src/app/products/products.service.ts
+++ front/src/app/products/products.service.ts
@@ -11,16 +11,16 @@ export class ProductsService {
     private static productslist: Product[] = null;
     private products$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
-    private url = 'http://127.0.0.1:8001/api/products';
+    private apiUrl = 'http://127.0.0.1:8001/api/products' ;
+    
 
     constructor(private http: HttpClient) { }
 
     getProducts(): Observable<Product[]> {
         if( ! ProductsService.productslist )
         {
-            this.http.get<any>(this.url).subscribe(data => {
+            this.http.get<any>(this.apiUrl).subscribe(data => {
                 ProductsService.productslist = data['hydra:member'];
-                console.log(data);
                 this.products$.next(ProductsService.productslist);
             });
         }
@@ -40,8 +40,10 @@ export class ProductsService {
         const headers = { 'content-type': 'application/json'}  
         const body=JSON.stringify(prod);
        
-        this.http.post<any>(this.url , body, {'headers': headers} ).subscribe(data => {
+        this.http.post<any>(this.apiUrl , body, {'headers': headers} ).subscribe(data => {
         });
+
+        this.products$.next(ProductsService.productslist = [prod, ...ProductsService.productslist]);
 
         return this.products$;
     }
@@ -49,20 +51,22 @@ export class ProductsService {
     update(prod: Product): Observable<Product[]>{
 
         this.products$.next(ProductsService.productslist);        
-        this.http.put<any>(this.url + prod.id, prod).subscribe(data => {
-           
+        this.http.put<any>(this.apiUrl + '/' + prod.id, prod).subscribe(data => {
     });
+
+        this.products$.next(ProductsService.productslist = [prod, ...ProductsService.productslist]);
 
         return this.products$;
     }
 
     delete(id: number): Observable<Product[]>{
 
-        this.http.delete<any>(this.url + id).subscribe(data => {
-        
+        this.http.delete<any>(this.apiUrl + '/' + id).subscribe(data => {  
     });
 
-    this.products$.next(ProductsService.productslist);
+    this.products$.next(ProductsService.productslist.filter(prod => prod.id != id));
+
     return this.products$;
+
     }
 }
